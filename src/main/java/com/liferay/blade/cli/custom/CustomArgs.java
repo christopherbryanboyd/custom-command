16b468a6282833e525d76a10baefbfaecb592ad7
Christopher Bryan Boyd <christopher.boyd@liferay.com>
2018-05-15 14:56:51 -0500

    Custom Command

---
+++ src/main/java/com/liferay/blade/cli/custom/CustomArgs.java
@@ -0,0 +1,8 @@
+package com.liferay.blade.cli.custom;
+import com.beust.jcommander.Parameters;
+import com.liferay.blade.cli.BaseArgs;
+
+@Parameters(commandNames="custom")
+public class CustomArgs extends BaseArgs {
+
+}
